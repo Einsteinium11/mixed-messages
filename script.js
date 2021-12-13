41b7e27bb8dc201b4f3e86e0604a8d6156eb71f7
Einsteinium <jbologna394@gmail.com>
2021-12-13 05:32:23 -0500

    Implement JavaScript Code

---
+++ script.js
@@ -0,0 +1,18 @@
+/* 
+This is a baseball team/quote generator! Every time it is used, it will generate a random quote!
+*/
+const teams = ['Mets', 'Braves', 'Nationals', 'Marlins', 'Phillies'];
+const hits = ['single', 'double', 'triple', 'homerun', 'groundrule double'];
+const positions = ['pitcher', 'catcher', 'first baseman', 'second baseman', 'third baseman', 'shortstop', 'left fielder', 'right fielder', 'center fielder'];
+const outs = ['flyout', 'strikeout', 'double play'];
+
+// Returns a random index from array
+const getRandIndex = array => Math.floor(Math.random() * array.length);
+
+let teamIndex = getRandIndex(teams);
+let positionIndex = getRandIndex(positions);
+let hitIndex = getRandIndex(hits);
+let outIndex = getRandIndex(outs)
+
+console.log(`Your team is the ${teams[teamIndex]}\n The ${positions[positionIndex]} will hit a ${hits[hitIndex]}\n Don't get a ${outs[outIndex]}!`);
+
